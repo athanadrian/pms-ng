86cbe9b272ddc6e379b64cb823c7d5470ff3e03f
athanadrian <athanadrian@gmail.com>
2018-07-29 13:29:23 +0200

    git cade add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { PropertyComponent } from './property/property.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { PropertyListItemComponent } from './property/property-list-item/property-list-item.component';
 
+const routes = [
+  { path: '', component: PropertyComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PropertyComponent,
+    PropertyListComponent,
+    PropertyListItemComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
