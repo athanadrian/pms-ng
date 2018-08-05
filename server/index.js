3fe2103838fd758f33443f8c9475f1e223006e0f
athanadrian <athanadrian@gmail.com>
2018-08-05 12:38:00 +0300

    server packages, connect to mLab mongo DB

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/dev')
+
+mongoose.connect(config.DB_URI);
 
 const app = express();
 
-app.get('/properties', function(req,res){
-    res.json('success:': true);
+app.get('/properties', function (req, res) {
+    res.json({ 'success:': true });
 })
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log('Server is running....');
 })
